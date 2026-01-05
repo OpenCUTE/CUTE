8bbe30de0cc6d30abda8ea7e88b47a43e59dcd84
Wenqing Li <liwenqing@ict.ac.cn>
2026-01-05 11:27:31 +0800

    Update scripts/setup-get-rvv-toolchain.sh

--- scripts/setup-get-rvv-toolchain.sh
+++ scripts/setup-get-rvv-toolchain.sh
@@ -9,9 +9,10 @@ echo "[Setup-Rvv-toolchain-step-1] CUTE root absolute path: $CUTE_ROOT_DIR"
 
 echo ""
 echo "[Setup-Rvv-toolchain-step-2] Download the precompiled RISC-V Vector (RVV) toolchain..."
+mkdir -p "$CUTE_ROOT_DIR/tool"
 cd "$CUTE_ROOT_DIR/tool"
 wget https://github.com/OpenCUTE/riscv-gnu-toolchain/releases/download/dac2026/rvvtoolchain.tar.xz
-tar -cJvf rvvtoolchain.tar.xz riscv
+tar -xJvf rvvtoolchain.tar.xz
 echo "[Setup-Rvv-toolchain-step-2] RVV toolchain download and extraction complete."
 echo ""
 
